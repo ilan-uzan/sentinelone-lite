cde4a9694d223977a464994fabc9c4d66c3de234
Ilan Uzan <ilanxuzan@gmail.com>
2025-08-14 21:39:24 +0300

    Fix frontend API connection: add CORS support and set VITE_API_BASE in Dockerfile

--- api/app/main.py
+++ api/app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost", "http://127.0.0.1"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
